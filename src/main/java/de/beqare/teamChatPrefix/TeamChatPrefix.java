7526a1d258b02d974cc40164d1b00209752a90b4
beqare <turtelmaster11@gmail.com>
2025-08-18 18:03:05 +0200

    Config Added

--- src/main/java/de/beqare/teamChatPrefix/TeamChatPrefix.java
+++ src/main/java/de/beqare/teamChatPrefix/TeamChatPrefix.java
@@ -1,5 +1,8 @@
 package de.beqare.teamChatPrefix;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.Team;
@@ -8,10 +11,11 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
 
-public final class TeamChatPrefix extends JavaPlugin implements Listener {
+public final class TeamChatPrefix extends JavaPlugin implements Listener, CommandExecutor {
 
     private String chatFormat;
     private boolean useTeamColor;
@@ -22,13 +26,14 @@ public final class TeamChatPrefix extends JavaPlugin implements Listener {
         saveDefaultConfig();
         loadConfig();
         getServer().getPluginManager().registerEvents(this, this);
+        Objects.requireNonNull(getCommand("tcp")).setExecutor(this);
         getLogger().info("TeamChatPrefix enabled");
     }
 
     private void loadConfig() {
+        reloadConfig();
         FileConfiguration config = getConfig();
 
-        // Default
         config.addDefault("format", "%teamcolor%%prefix%%playername%%suffix%§8 » §7%message%");
         config.addDefault("use-team-color", true);
         config.addDefault("default-color", "§f");
@@ -38,6 +43,30 @@ public final class TeamChatPrefix extends JavaPlugin implements Listener {
         chatFormat = Objects.requireNonNull(config.getString("format"));
         useTeamColor = config.getBoolean("use-team-color");
         defaultColor = ChatColor.getByChar(Objects.requireNonNull(config.getString("default-color")).replace("§", "").charAt(0));
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, Command cmd, @NotNull String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("tcp")) {
+            if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
+                if (!sender.hasPermission("teamchatprefix.reload") && !sender.isOp()) {
+                    sender.sendMessage(ChatColor.RED + "You don't have permission to reload the config!");
+                    return true;
+                }
+
+                try {
+                    loadConfig();
+                    sender.sendMessage(ChatColor.GREEN + "TeamChatPrefix config reloaded successfully!");
+                } catch (Exception e) {
+                    sender.sendMessage(ChatColor.RED + "Error while reloading config: " + e.getMessage());
+                    getLogger().severe("Error reloading config: " + e.getMessage());
+                }
+                return true;
+            }
+            sender.sendMessage(ChatColor.YELLOW + "Usage: /tcp reload");
+            return true;
+        }
+        return false;
     }
 
     @EventHandler
