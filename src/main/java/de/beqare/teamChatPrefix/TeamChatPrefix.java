0c3e572d4a44b431c2872318bc8b1c527b95bddc
beqare <turtelmaster11@gmail.com>
2025-08-18 17:40:42 +0200

    Config Added

--- src/main/java/de/beqare/teamChatPrefix/TeamChatPrefix.java
+++ src/main/java/de/beqare/teamChatPrefix/TeamChatPrefix.java
@@ -7,18 +7,37 @@ import org.bukkit.ChatColor;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.configuration.file.FileConfiguration;
+
+import java.util.Objects;
 
 public final class TeamChatPrefix extends JavaPlugin implements Listener {
 
-    @Override
-    public void onEnable() {
-        getLogger().info("TeamChatPrefix enabled");
-        getServer().getPluginManager().registerEvents(this, this);
-    }
+    private String chatFormat;
+    private boolean useTeamColor;
+    private ChatColor defaultColor;
 
     @Override
-    public void onDisable() {
-        getLogger().info("TeamChatPrefix disabled");
+    public void onEnable() {
+        saveDefaultConfig();
+        loadConfig();
+        getServer().getPluginManager().registerEvents(this, this);
+        getLogger().info("TeamChatPrefix enabled");
+    }
+
+    private void loadConfig() {
+        FileConfiguration config = getConfig();
+
+        // Default
+        config.addDefault("format", "%teamcolor%%prefix%%playername%%suffix%§8 » §7%message%");
+        config.addDefault("use-team-color", true);
+        config.addDefault("default-color", "§f");
+        config.options().copyDefaults(true);
+        saveConfig();
+
+        chatFormat = Objects.requireNonNull(config.getString("format"));
+        useTeamColor = config.getBoolean("use-team-color");
+        defaultColor = ChatColor.getByChar(Objects.requireNonNull(config.getString("default-color")).replace("§", "").charAt(0));
     }
 
     @EventHandler
@@ -26,18 +45,24 @@ public final class TeamChatPrefix extends JavaPlugin implements Listener {
         Player player = event.getPlayer();
         Team team = player.getScoreboard().getEntryTeam(player.getName());
 
-        String prefix = "";
-        String suffix = "";
-        ChatColor color = ChatColor.WHITE;
+        String teamName = team != null ? team.getName() : "";
+        String prefix = team != null ? team.getPrefix() : "";
+        String suffix = team != null ? team.getSuffix() : "";
+        ChatColor color = (useTeamColor && team != null) ? team.getColor() : defaultColor;
 
-        if (team != null) {
-            prefix = team.getPrefix();
-            suffix = team.getSuffix();
-            color = team.getColor();
-        }
+        String formattedMessage = chatFormat
+                .replace("%playername%", player.getName())
+                .replace("%team%", teamName)
+                .replace("%prefix%", prefix)
+                .replace("%suffix%", suffix)
+                .replace("%teamcolor%", color.toString())
+                .replace("%message%", event.getMessage());
 
-        String format = color + prefix + "%1$s" + suffix + ChatColor.DARK_GRAY + " » " + ChatColor.GRAY + "%2$s";
+        event.setFormat(formattedMessage);
+    }
 
-        event.setFormat(format);
+    @Override
+    public void onDisable() {
+        getLogger().info("TeamChatPrefix disabled");
     }
 }
